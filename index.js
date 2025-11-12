75331b013a5c21f2df51c89fee16ba9e4e5137a7
glitch86 <mdpolokahmed825@gmail.com>
2025-11-12 12:11:33 +0600

    added id specific movie api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.n1li6q4.mongodb.net/?appName=Cluster0`;
 const port = process.env.PORT || 3000;
@@ -24,10 +24,21 @@ async function run() {
     await client.connect();
     const db = client.db("movieMaster");
     const moviesCollection = db.collection("movies");
-
+// load all movies 
     app.get("/movies", async(req, res) => {
       const result = await moviesCollection.find().toArray();
       res.send(result);
+    });
+
+
+    // load id specific movie
+     
+    app.get('/movies/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await moviesCollection.findOne(query);
+      res.send(result);
+      
     });
   } finally {
     // Ensures that the client will close when you finish/error
